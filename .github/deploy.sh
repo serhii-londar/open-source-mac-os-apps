442329d299031bcba92ce1593fdf37c9f47a6bdc
Serhii Londar <serhii.londar@gmail.com>
2018-11-15 01:12:30 +0200

    CI Fix

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -8,7 +8,7 @@ then
   exit
 fi
 
-git checkout master
+git checkout readme-generation
 
 git config user.name "serhii-londar"
 git config user.email "serhii.londar@gmail.com"
@@ -20,4 +20,4 @@ echo commit
 git commit -m "Generate README"
 
 echo push
-git push --quiet "https://${DANGER_GITHUB_API_TOKEN}@github.com/serhii-londar/open-source-mac-os-apps.git" master:master > /dev/null 2>&1
+git push --quiet "https://${DANGER_GITHUB_API_TOKEN}@github.com/serhii-londar/open-source-mac-os-apps.git"  readme-generation:readme-generation > /dev/null 2>&1
